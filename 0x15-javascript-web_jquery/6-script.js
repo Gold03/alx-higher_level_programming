72deefcb42579bc25aa6515c03deab858646c971
Tife_dev <85970250+Gold03@users.noreply.github.com>
2022-12-13 15:14:50 +0100

    Create 6-script.js

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+/*updates the text of the <header> element to New Header!!! 
+when the user clicks on DIV#update_header*/
+$("#update_header").click(function(){
+    $("header").text("New Header!!!");
+});
